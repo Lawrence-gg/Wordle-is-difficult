4d127c860fcfb2f4944af579ff841d6499197315
Lawrence Goodhead <lawrence-g@Lawrences-MacBook-Air.local>
2022-04-20 23:24:05 +1200

    initial commit; initialise db and set up wordlist

---
+++ seeds/words-list.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const fs = require('fs')
+
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('dictionary').del()
+  const words = fs
+    .readFileSync('server/public/data/wordfile.txt', 'utf-8')
+    .split('\n')
+  let emptyArr = []
+  for (let i = 0; i < words.length; i++) {
+    emptyArr.push(words[i])
+    await knex('dictionary').insert({ word: emptyArr[i] })
+  }
+  console.log('Completed')
+}
+// new comment
