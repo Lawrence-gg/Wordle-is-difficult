c8e7677cc9471cf537f22571955db1fcc86a8942
Lawrence Goodhead <lawrence-g@Lawrences-MacBook-Air.local>
2022-04-21 14:30:03 +1200

    Testing some basic mui components for my layout

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Grid,Paper, Container} from '@material-ui/core'
+
 
 const Game = () => {
-  return <div className="game">
-    <div className="game"></div>
+  return (
+  <div className="game">
+    <Container>
+      <Grid container>
+        <Grid item md={1}>
+          <Paper>Save</Paper>
+        </Grid>
+        <Grid item md={1}>
+          <Paper>Elate</Paper>
+        </Grid>
+        <Grid item>
+          <Paper>C</Paper>
+        </Grid>
+      </Grid>
+    </Container>
   </div>
+  )
 }
 
 export default Game
