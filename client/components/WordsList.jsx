be1e3797b769cc11357702e06cc8df48a27614e9
Lawrence Goodhead <92251452+Lawrence-gg@users.noreply.github.com>
2022-04-26 00:05:37 +1200

    returning inital 5700 words as paper items

--- client/components/WordsList.jsx
+++ client/components/WordsList.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { Grid, Paper, Container } from '@material-ui/core'
+import { getTestWords } from '../api/wordApi'
 
 const WordsList = () => {
+  const [dictionary, setDictionary] = useState([])
+
+  const handleClick = () => {
+    console.log(dictionary)
+  }
+  
+  useEffect(() => {
+    getTestWords().then((words) => {
+      setDictionary(...dictionary,words)
+    })
+  },[])
+
   return (
     <div>
+    <button onClick={handleClick}>Populate</button>
+    {/* {dictionary.map((word, idx) => <div key={idx}><p>{word.word}</p></div>)} */}
+  
       <Container>
-        <Grid container>
-          <Grid item md={1}>
-            <Paper>Save</Paper>
-          </Grid>
+      <Grid container>
+      {dictionary.map((word, i) => {
+        return ( <Grid item md={1} key ={i}>
+            <Paper>{word.word}</Paper>
+          </Grid>)
+      })}
           <Grid item md={1}>
             <Paper>Elate</Paper>
           </Grid>
